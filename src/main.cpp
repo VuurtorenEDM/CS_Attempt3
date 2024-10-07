16d62b226570e263898da087d1f3143b03db7a20
Martin-code-art <matinshiiran@gmail.com>
2024-10-07 16:00:58 +0200

    Blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <Arduino.h>
 
 // test
-// put function declarations here:
+int ledpin = 13;
+
 int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once: please heckin work
-  int result = myFunction(2, 3);
+void setup()
+{
+  pinmode(ledpin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  myFunction();
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
-  return x + y;
+  digitalwrite(ledpin, HIGH);
+  delay(1000);
+  digitalwrite(ledpin, LOW);
+  delay(1000);
 }
