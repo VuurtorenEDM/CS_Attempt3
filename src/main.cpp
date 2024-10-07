23eb37e742d92d71b0ade43c9d89e01d0777c9db
Alexia220700 <alexia.mit@student.nhlstenden.com>
2024-10-07 15:52:58 +0200

    Sum

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,25 @@
 
 // test
 // put function declarations here:
-int myFunction(int, int);
+void Sum();
 
 void setup() {
   // put your setup code here, to run once: please heckin work
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+  //int result = myFunction(2, 3);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int Sum() {
+  Serial.println("Provide the first number: ");
+  int x = Serial.parseInt();
+
+  Serial.println("Provide the second number: ");
+  int y = Serial.parseInt();
+
+  int sum = y + x;
+
+  Serial.print("Sum is: ");
+  Serial.println(sum);
+
+  showmenu();
 }
