81d3d81a7fc7f8a503de3224f40b0aa1827eeded
Nannerscube <72697207+Nannerscube@users.noreply.github.com>
2024-10-09 10:12:50 +0200

    Update main.cpp
    Multi and menu added

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <Arduino.h>
 
+<<<<<<< Updated upstream
 // test
 // put function declarations here:
 int myFunction(int, int);
+=======
+// Function declarations
+void showMenu();
+void Multiplication();
+>>>>>>> Stashed changes
 
 void setup() {
   // put your setup code here, to run once: please heckin work
@@ -10,6 +16,7 @@ void setup() {
 }
 
 void loop() {
+<<<<<<< Updated upstream
   // put your main code here, to run repeatedly:
 }
 
@@ -19,3 +26,45 @@ int myFunction(int x, int y) {
 }
 
 
+=======
+  if (Serial.available() > 0) {
+    // Read the user input
+    char option = Serial.read();
+    
+    // Check option
+    switch (option) {
+      case '1':
+        Sum();
+        break;
+      case '2':
+        Multiplication();
+        break;
+      default:
+        Serial.println("Invalid option. 1 or 2.");
+        showMenu();  // Show the menu again if its invalid
+        break;
+    }
+  }
+}
+
+// display the menu
+void showMenu() {
+  Serial.println("1: Calculate Sum");
+  Serial.println("2: Calculate Multiplication");
+  Serial.println("1 or 2? ");
+}
+// Multiplication Function
+void Multiplication() 
+{
+  // get 2 integers from input, return the numbers
+  Serial.println("Please Provide the First Number: ");
+  int num1 = Serial.parseInt();
+  Serial.println("Please Provide the Second Number: ");
+  int num2 = Serial.parseInt();
+  int Multiplied = num1 * num2;
+  Serial.print("The Result Is: ");
+  Serial.println(Multiplied);
+
+  showmenu();
+}
+>>>>>>> Stashed changes
