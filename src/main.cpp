7be55bd9a1e3f5b352567b49935ac06a416eaed4
Alexia220700 <alexia.mit@student.nhlstenden.com>
2024-10-09 11:05:22 +0200

    Original Sum

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,13 @@
 #include <Arduino.h>
 
-// test
-// put function declarations here:
 void Sum();
 
-void setup() {
-  // put your setup code here, to run once: please heckin work
-  //int result = myFunction(2, 3);
-}
-
-// put function definitions here:
-int Sum() {
+//used to make a sum of two numbers 
+void Sum() 
+{
   Serial.println("Provide the first number: ");
   int x = Serial.parseInt();
-
+  
   Serial.println("Provide the second number: ");
   int y = Serial.parseInt();
 
